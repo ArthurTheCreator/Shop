8080ad2c07c219dc51d174c25221d76d6479f3ef
Arthur Bomfim <arthurbao10@gmail.com>
2025-02-06 11:18:40 -0300

    a

--- Shop.Web/obj/Debug/net8.0/Shop.Web.AssemblyInfo.cs
+++ Shop.Web/obj/Debug/net8.0/Shop.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shop.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cf53e11cb3df1e43beee926e76a9a53ecd7eec7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a66a1778bd5aa6c5483552de8d12c221f0a7991")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shop.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shop.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
